07080eeabde9b5c734e34eb0626d266e5457e34d
Boone Epstein <booneepstein@gmail.com>
2021-02-19 23:46:23 -0600

    Make queue draggable

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import QueuedTimer from './QueuedTimer'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const Schedule = ({timers, removeTimer}) => {
-  const queue = timers.map(timer =>
-    <QueuedTimer
-      key={timer.id}
-      timer={timer}
-      removeTimer={removeTimer}
-    />
-  )
+  const queue = timers.map((timer, index) => {
+    return (
+      <Draggable
+        key={timer.id}
+        draggableId={timer.id.toString()}
+        index={index}
+      >
+        {(provided) => (
+          <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+            <QueuedTimer
+              timer={timer}
+              removeTimer={removeTimer}
+            />
+          </li>
+        )}
+      </Draggable>
+    )
+  })
+
   return (
-    <div className='schedule'>
-      {queue}
-    </div>
+    <DragDropContext>
+      <Droppable droppableId="timers">
+        {(provided) => (
+          <ul
+            className='schedule'
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {queue}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
   )
 }
 
