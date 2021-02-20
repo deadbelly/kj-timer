752f92d22764be1642b2db8f3468701dc16fbe7d
Boone Epstein <booneepstein@gmail.com>
2021-02-19 23:15:43 -0600

    Add api calls to ActiveTimer

--- src/components/ActiveTimer.js
+++ src/components/ActiveTimer.js
@@ -1,27 +1,46 @@
 import React from 'react'
 import Countdown, { zeroPad } from 'react-countdown'
 
-const ActiveTimer = ({timer, clearTimer}) => {
-  const timerMilli = timer.seconds * 1000
-
-  const renderer = ({ minutes, seconds, completed }) => {
-  if (completed) {
-    return <h1>DONE</h1>
-  } else {
-    return <span>{zeroPad(minutes)}:{zeroPad(seconds)}</span>;
+class ActiveTimer extends React.Component {
+  constructor() {
+    super()
   }
-}
 
-  return (
-    <main>
-      <h2>{timer.title}</h2>
-      <Countdown
-        date={Date.now() + timerMilli}
-        renderer={renderer}
-        onComplete={clearTimer}
-      />
-    </main>
-  )
+  shouldComponentUpdate(nextProps) {
+    const {timer} = this.props
+
+    return timer.seconds !== nextProps.timer.seconds
+  }
+
+  render() {
+    const {timer, clearTimer} = this.props
+
+    const timerMilli = timer.seconds * 1000
+
+    const renderer = ({ minutes, seconds, api }) => {
+      const { start, pause } = api
+      return (
+        <>
+          <h1>{zeroPad(minutes)}:{zeroPad(seconds)}</h1>
+          <div className='controls'>
+            <button onClick={start}>START</button>
+            <button onClick={pause}>PAUSE</button>
+          </div>
+        </>
+      )
+    }
+
+    return (
+      <main>
+        <h2>{timer.title}</h2>
+        <Countdown
+          date={Date.now() + timerMilli}
+          renderer={renderer}
+          onComplete={clearTimer}
+        />
+      </main>
+    )
+  }
 }
 
 export default ActiveTimer
