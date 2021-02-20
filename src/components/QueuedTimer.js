e1472c06204f39387fdea6f2ccbf39529419813d
Boone Epstein <booneepstein@gmail.com>
2021-02-20 13:21:24 -0600

    Add time formater to queued timers

--- src/components/QueuedTimer.js
+++ src/components/QueuedTimer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import timeFormater from '../timeFormater'
 
 const QueuedTimer = ({timer, removeTimer}) => {
   const {title, seconds, id} = timer
   return (
     <div className="queued-timer">
       <h4>{title}</h4>
-      <p>{seconds}</p>
+      <p>{timeFormater.format(seconds)}</p>
       <button onClick={() => removeTimer(id)}>REMOVE</button>
     </div>
   )
